4fdd2274232a35547f20e763c8bbb8dfbd91254b
LiBaoXu <libx@inke.cn>
2018-05-10 16:21:50 +0800

    [build] 1.0.2

--- build/release.sh
+++ build/release.sh
@@ -1,4 +1,4 @@
-git checkout -b master
+git checkout master
 git merge dev
 
 #!/usr/bin/env sh
@@ -30,7 +30,7 @@ then
   # publish
   git push origin refs/tags/v$VERSION
   git push origin master
-  git checkout -b dev
+  git checkout dev
   git rebase master
   git push origin dev
 fi
