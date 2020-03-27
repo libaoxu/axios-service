ba8370ff154c0c1909d8effe158e759fef73b185
李宝旭 <libx@inke.cn>
2020-03-27 15:45:55 +0800

    delete ik files

--- ik.deploy.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  {
-    enable: true,                        // 发布任务开关，为true才会进行发布
-    name: 'webcdn.inke.cn',              // 发布任务名称，可以随便起
-    url: 'https://deploy.inkept.cn/templates/deploy.html?job_name=cop.inke_owt.bpc_pdl.staticresources_servicegroup.static_service.resources_job.resources_cluster.ali-bj' 	// 发布任务地址，就是发布系统上的发布任务地址
-  }
-];
